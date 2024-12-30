6e49b5d941a5bd81c0ce173932ba2cbc54e52661
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-30 14:24:49 -0300

    implement solution

---
+++ part2/CountingToHundred.java
@@ -0,0 +1,18 @@
+package part2;
+
+
+import java.util.Scanner;
+
+public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = input; i <= 100; i++) {
+            System.out.println(i);
+        }
+        scanner.close();
+    }
+}
