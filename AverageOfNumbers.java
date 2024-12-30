b1ccfeef9bc722c32e2e1a08f66a556ac5061aa0
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-30 12:26:50 -0300

    implement solution

---
+++ AverageOfNumbers.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+
+        while (true) {
+
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number < 0 || number > 0) {
+                sum += number;
+                count++;
+            }
+        }
+
+        double avg = (double) sum/count;
+        System.out.println("Average of the numbers: " + avg);
+    }
+}
