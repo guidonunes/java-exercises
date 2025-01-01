5d9b481b29086317de7e0bee9b733ea9fa00a14d
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-01 18:34:15 -0300

    implement exercise

---
+++ part2/FromParameterToOne.java
@@ -0,0 +1,17 @@
+package part2;
+
+public class FromParameterToOne {
+
+  public static void main(String[] args) {
+      printFromNumberToOne(5);
+
+  }
+
+  public static void printFromNumberToOne(int number) {
+      while (number > 0) {
+          System.out.println(number);
+          number -= 1;
+      }
+
+  }
+}
