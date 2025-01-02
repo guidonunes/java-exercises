27fb6eb476f7c54eecb581ec14a1dd4d0dce76f0
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-02 14:58:07 -0300

    implement exercise solution

---
+++ part2/Word.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String quote = word();
+        System.out.println(quote);
+
+    }
+
+    public static String word() {
+        return "May the Force be with you.";
+    }
+
+}
