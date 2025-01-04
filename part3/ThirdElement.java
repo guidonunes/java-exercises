c9542c889de8d934ce9c7c0ce0c1af8fb468e4ac
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-04 15:47:56 -0300

    add exercise solution and directory

---
+++ part3/ThirdElement.java
@@ -0,0 +1,32 @@
+package part3;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ThirdElement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            System.out.print("Enter a string: ");
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+        list.add("Tom");
+        list.add("Emma");
+        list.add("Alex");
+        list.add("Mary");
+        list.add("Olga");
+
+
+        System.out.println(list.get(2));
+        scanner.close();
+    }
+}
