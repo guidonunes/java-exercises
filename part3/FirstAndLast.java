9036f542c97a915d47b5ba4b0bfa483f2f7e6305
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-05 22:46:51 -0300

    implement exercise solution

---
+++ part3/FirstAndLast.java
@@ -0,0 +1,36 @@
+package part3;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+        int index = 0;
+        while (index < list.size()) {
+            String name = list.get(index);
+            System.out.println(name);
+            break;
+        }
+
+        int indexLast = list.size()-1;
+        while (indexLast >= 0) {
+            String name = list.get(indexLast);
+            System.out.println(name);
+            break;
+        }
+        scanner.close();
+    }
+}
