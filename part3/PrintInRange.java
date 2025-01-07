c3baeff9097febc88a8d0a8a19f6e54871000164
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-07 10:26:47 -0300

    implement exercise solution

---
+++ part3/PrintInRange.java
@@ -0,0 +1,35 @@
+package part3;
+
+
+import java.util.ArrayList;
+
+public class PrintInRange {
+
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int value: numbers) {
+            if(lowerLimit <= value && value <= upperLimit) {
+                System.out.println(value);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+    }
+
+}
