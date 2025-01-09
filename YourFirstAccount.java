c23693c0566a56f98b2e4893a0b860de04e4500d
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-09 15:54:19 -0300

    implement solution

---
+++ YourFirstAccount.java
@@ -0,0 +1,15 @@
+
+
+public class YourFirstAccount {
+
+  public static void main(String[] args) {
+      Account nineAccount = new Account("Janine's account", 100.00);
+
+      System.out.println(nineAccount);
+
+      nineAccount.deposit(20);
+
+      System.out.println(nineAccount);
+
+  }
+}
