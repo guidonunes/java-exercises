a3ef1b9596b9757f4e5841cf18a558359ef1c168
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-09 00:06:25 -0300

    implement solution

---
+++ part3/LineByLine.java
@@ -0,0 +1,28 @@
+package part3;
+
+import java.util.Scanner;
+
+public class LineByLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] pieces = input.split(" ");
+
+            for (String piece : pieces) {
+                System.out.println(piece);
+            }
+
+            System.out.print("");
+        }
+
+        scanner.close();
+    }
+}
