2143a597bdbc79ccf5c0de27092061ad35af32f6
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-09 10:53:20 -0300

    implement solution

---
+++ part3/LastWords.java
@@ -0,0 +1,29 @@
+package part3;
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] pieces = input.split(" ");
+
+            for(int i = 0; i < pieces.length; i++) {
+                if(i == pieces.length -1) {
+                    System.out.println(pieces[i]);
+                }
+            }
+
+        }
+        scanner.close();
+
+    }
+}
