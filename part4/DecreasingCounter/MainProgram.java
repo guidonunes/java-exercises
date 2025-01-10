55c795d9eadc580c1adbc1c4e84ba8e9ed0751a8
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-10 10:48:49 -0300

    implement solution

---
+++ part4/DecreasingCounter/MainProgram.java
@@ -0,0 +1,17 @@
+package part4.DecreasingCounter;
+
+public class MainProgram {
+
+  public static void main(String[] args) {
+      DecreasingCounter counter = new DecreasingCounter(100);
+
+      counter.printValue();
+
+      counter.decrement();
+      counter.printValue();
+
+      counter.reset();
+      counter.printValue();
+//        counter.decrement();
+  }
+}
