d998e51bc53cabf9e023bd6cd2186fde33edacfa
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-14 09:45:09 -0300

    implement exercise solution

---
+++ part4/Programs/Main.java
@@ -0,0 +1,39 @@
+package part4.Programs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the TelevisionProgram class
+
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if(name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+
+        }
+
+        System.out.print ("Program's maximum duration? ");
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (TelevisionProgram program : programs) {
+            if (program.getDuration() <= input) {
+                System.out.println(program);
+            }
+        }
+        scanner.close();
+    }
+}
