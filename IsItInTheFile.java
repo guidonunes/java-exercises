f5176df3326454a930c8a0957740eebf1f28d379
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-15 15:13:39 -0300

    implemented exercise solution

---
+++ IsItInTheFile.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class IsItInTheFile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        System.out.println("Search for:");
+        String searchedFor = scanner.nextLine();
+
+
+        try(Scanner read = new Scanner(Paths.get(file))) {
+            while(read.hasNextLine()) {
+                String line = read.nextLine();
+                list.add(line);
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Reading the file " + file + " failed.");
+            return;
+        }
+        scanner.close();
+
+         while(true) {
+            if(searchedFor.isEmpty()) {
+                return;
+            }
+
+            if (!(list.contains(searchedFor))) {
+                System.out.println("Not found.");
+                return;
+            } else {
+                System.out.println("Found!");
+                return;
+            }
+        }
+
+    }
+}
