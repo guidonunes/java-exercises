ccd63d52eb56a70951b5d35ea6bd28d12d98ee7f
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-19 15:55:19 -0300

    implement exercise solution

--- part5/Counter/MainProgram.java
+++ part5/Counter/MainProgram.java
@@ -1,5 +1,14 @@
 package part5.Counter;
 
+
 public class MainProgram {
 
+  public static void main(String[] args) {
+      Counter counter = new Counter(0);
+      counter.increase();
+      counter.increase(100);
+      counter.decrease(99);
+
+      System.out.println(counter.value());
+  }
 }
---
+++ part5/HealthStation/HealthStation.java
@@ -0,0 +1,26 @@
+package part5.HealthStation;
+
+
+public class HealthStation {
+  private int weighingsCount;
+
+  public HealthStation() {
+      this.weighingsCount = 0;
+  }
+
+
+  public int weigh(Person person) {
+      this.weighingsCount++;
+      return person.getWeight();
+
+  }
+
+  public void feed(Person person) {
+      person.setWeight(person.getWeight() + 1);
+  }
+
+  public int weighings() {
+      return this.weighingsCount;
+  }
+
+}
