d997dbdbf9087235af0ad5c5cfafb82718875f02
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-20 16:50:53 -0300

    implement exercise solution

---
+++ part5/Apartment/Apartment.java
@@ -0,0 +1,40 @@
+package part5.Apartment;
+
+public class Apartment {
+
+  private int rooms;
+  private int squares;
+  private int pricePerSquare;
+
+  public Apartment(int rooms, int squares, int pricePerSquare) {
+      this.rooms = rooms;
+      this.squares = squares;
+      this.pricePerSquare = pricePerSquare;
+  }
+
+  public int getPrice() {
+      return this.squares * this.pricePerSquare;
+  }
+
+  public boolean largerThan(Apartment compared) {
+      if(this.getPrice() > compared.getPrice()) {
+          return true;
+      }
+      return false;
+  }
+
+  public int priceDifference(Apartment compared) {
+      if (this.getPrice() > compared.getPrice()) {
+          return this.getPrice() - compared.getPrice();
+      } else {
+          return compared.getPrice() - this.getPrice();
+      }
+  }
+
+  public boolean moreExpensiveThan(Apartment compared) {
+      if(this.getPrice() > compared.getPrice()) {
+          return true;
+      }
+      return false;
+  }
+}
---
+++ part5/Apartment/Main.java
@@ -0,0 +1,18 @@
+package part5.Apartment;
+
+public class Main {
+
+  public static void main(String[] args) {
+      // you can write code here to try out your program
+      Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+      Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+      Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+
+//        System.out.println("The total value is: " + manhattanStudioApt.getPrice());
+//        System.out.println("The total value is: " + atlantaTwoBedroomApt.getPrice());
+//        System.out.println("The total value is: " + bangorThreeBedroomApt.getPrice());
+
+      System.out.println(manhattanStudioApt.moreExpensiveThan(atlantaTwoBedroomApt));  // false
+      System.out.println(bangorThreeBedroomApt.moreExpensiveThan(atlantaTwoBedroomApt));   // true   //35400
+  }
+}
