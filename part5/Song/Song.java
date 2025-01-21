6a25ad1b1ae4cd7b57fc0812ab334a6c8fba374c
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-21 09:17:25 -0300

    implement exercise solution

---
+++ part5/Song/Song.java
@@ -0,0 +1,51 @@
+package part5.Song;
+
+public class Song {
+
+  private String artist;
+  private String name;
+  private int durationInSeconds;
+
+  public Song(String artist, String name, int durationInSeconds) {
+      this.artist = artist;
+      this.name = name;
+      this.durationInSeconds = durationInSeconds;
+  }
+
+  public String getArtist() {
+      return this.artist;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+  public int getDurationInSeconds() {
+      return this.durationInSeconds;
+  }
+
+
+  public boolean equals(Object compared) {
+      if (this == compared) {
+          return true;
+      }
+
+      if(!(compared instanceof Song)) {
+          return false;
+      }
+
+      Song comparedSong = (Song) compared;
+
+      if(this.artist.equals(comparedSong.artist) &&
+         this.name == comparedSong.name &&
+         this.durationInSeconds == comparedSong.durationInSeconds) {
+         return true;
+      }
+
+      return false;
+  }
+
+  @Override
+  public String toString() {
+      return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+  }
