b83e0719a8052e4653f61e5f5a536de4e00fd15c
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-23 13:46:24 -0300

    implement exercise solution

---
+++ part6/Longest/SimpleCollection.java
@@ -0,0 +1,42 @@
+package part6.Longest;
+
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+
+    public String longest() {
+        if(this.elements.isEmpty()) {
+            return null;
+        }
+
+
+
+        String longestWord = this.elements.get(0);
+
+        for(String longest : this.elements) {
+            if(longestWord.length() < longest.length()) {
+                longestWord = longest;
+            }
+
+        }
+        return longestWord;
+    }
+
+}
