8b52eb191cf87c13a1be6f91047ea9e4b488045b
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-24 18:51:38 -0300

    add code to component

--- part6/Dictionary/Main.java
+++ part6/Dictionary/Main.java
@@ -1,5 +1,17 @@
 package part6.Dictionary;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        // Try out how the different parts of the program work together
+        // here
+        Scanner scanner = new Scanner(System.in);
+        SimpleDictionary dictionary = new SimpleDictionary();
+
+        TextUI textUI = new TextUI(scanner, dictionary);
+        textUI.start();
+        // prints the string "hauki"
+    }
 }
--- part6/Dictionary/SimpleDictionary.java
+++ part6/Dictionary/SimpleDictionary.java
@@ -1,5 +1,20 @@
 package part6.Dictionary;
 
+import java.util.HashMap;
+
 public class SimpleDictionary {
 
+    private HashMap<String, String> translations;
+
+    public SimpleDictionary() {
+        this.translations = new HashMap<>();
+    }
+
+    public String translate(String word) {
+        return this.translations.get(word);
+    }
+
+    public void add(String word, String translation) {
+        this.translations.put(word, translation);
+    }
 }
