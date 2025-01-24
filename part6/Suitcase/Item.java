89f958ffd44a8c0fbf67549bb7d8a029e6e71be8
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-24 14:48:20 -0300

    add item class

---
+++ part6/Suitcase/Item.java
@@ -0,0 +1,24 @@
+package part6.Suitcase;
+
+
+public class Item {
+  private String name;
+  private int weight;
+
+  public Item(String name, int weight) {
+      this.name = name;
+      this.weight = weight;
+  }
+
+  public String getName() {
+      return this.name;
+  }
+
+  public int getWeight() {
+      return this.weight;
+  }
+
+  public String toString() {
+      return this.name + " (" + this.weight + " kg" +")";
+  }
+}
