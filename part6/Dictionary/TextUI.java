af01d25b527a16df26f51dbe7de63fb15e4810f4
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-24 22:57:42 -0300

    add code to component

--- part6/Dictionary/TextUI.java
+++ part6/Dictionary/TextUI.java
@@ -1,5 +1,52 @@
 package part6.Dictionary;
 
-public class TextUI {
 
+import java.util.Scanner;
+
+public class TextUI {
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+        while(true) {
+            System.out.println("Command:");
+            String word = scanner.nextLine();
+
+            if(word.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            }
+
+            if(word.equals("add")) {
+
+                System.out.println("Word:");
+                String input = scanner.nextLine();
+                System.out.println("Translation: ");
+                String translation = scanner.nextLine();
+
+                this.dictionary.add(input, translation);
+
+            } else if(word.equals("search")) {
+                System.out.print("To be translated: ");
+                String input = scanner.nextLine();
+
+                String translation = this.dictionary.translate(input);
+
+                if (translation == null) {
+                    System.out.println("Word " + input + " was not found!");
+                } else {
+                    System.out.println("Translation: " + translation);
+                }
+
+            } else {
+                System.out.println("Unknown command");
+                continue;
+            }
+        }
+    }
 }
