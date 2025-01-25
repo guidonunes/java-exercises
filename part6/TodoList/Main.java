08aad52c976735fcbf775c7c66bbb7dad6ed5326
Guilherme Nunes <guilherme.augd@gmail.com>
2025-01-24 22:58:54 -0300

    add code to component

---
+++ part6/TodoList/Main.java
@@ -0,0 +1,21 @@
+package part6.TodoList;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Here you can try out the combined functionality of your classes
+        Scanner scanner = new Scanner(System.in);
+        TodoList list = new TodoList();
+
+        UserInterface user = new UserInterface(list, scanner);
+
+        user.start();
+    }
+}
+
+
+}
