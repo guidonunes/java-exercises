7441ec978abe1aa080f77f5902be3c938133c111
Guilherme Nunes <guilherme.augd@gmail.com>
2025-02-06 15:32:57 -0300

    implement exercise solution

---
+++ part8/IOweYou/IOU.java
@@ -0,0 +1,21 @@
+package IOweYou;
+
+import java.util.HashMap;
+
+public class IOU {
+    private HashMap<String, Double> iouMap;
+
+
+    public IOU() {
+        this.iouMap = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        iouMap.put(toWhom, amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom) {
+        return iouMap.getOrDefault(toWhom, 0.0);
+    }
+
+}
