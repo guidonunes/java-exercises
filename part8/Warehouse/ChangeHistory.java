18e29f588bd93c7f114cd7f57949b105b912ea00
Guilherme Nunes <guilherme.augd@gmail.com>
2025-02-15 16:49:10 -0300

    add solution of the step 4/7

--- part8/Warehouse/ChangeHistory.java
+++ part8/Warehouse/ChangeHistory.java
@@ -1,5 +1,3 @@
-package Warehouse;
-
 import java.util.ArrayList;
 
 public class ChangeHistory {
@@ -17,8 +15,51 @@ public class ChangeHistory {
         this.history.clear();
     }
 
+    public double maxValue() {
+        if(this.history.isEmpty()) {
+            return 0.0;
+        }
+
+        double max = this.history.get(0);
+        for (double value : this.history) {
+            if(value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public double minValue() {
+        if(this.history.isEmpty()) {
+            return 0.0;
+        }
+
+        double min = this.history.get(0);
+        for(double value : this.history) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public double average() {
+        if(this.history.isEmpty()) {
+            return 0.0;
+        }
+
+        double sum = 0.0;
+        for (double num : this.history) {
+            sum += num;
+        }
+
+        return sum/this.history.size();
+    }
+
+
     @Override
     public String toString() {
         return this.history.toString();
     }
+
 }
