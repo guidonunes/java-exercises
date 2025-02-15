525f2173366d46ccb0ea85034bbdee3f599b7beb
Guilherme Nunes <guilherme.augd@gmail.com>
2025-02-15 15:30:36 -0300

    add solution of the step 1/7

---
+++ part8/Warehouse/Main.java
@@ -0,0 +1,15 @@
+package Warehouse;
+
+
+
+public class Main {
+
+  public static void main(String[] args) {
+      ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+      juice.addToWarehouse(1000.0);
+      juice.takeFromWarehouse(11.3);
+      System.out.println(juice.getName()); // Juice
+      System.out.println(juice);
+  }
+
+}
---
+++ part8/Warehouse/ProductWarehouse.java
@@ -0,0 +1,18 @@
+package Warehouse;
+
+
+public class ProductWarehouse extends Warehouse {
+
+  private String productName;
+
+  public ProductWarehouse(String productName, double capacity) {
+      super(capacity);
+      this.productName = productName;
+  }
+
+  public String getName() {
+      return this.productName;
+  }
+
+
+}
