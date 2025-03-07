706ba5875a5952eb7a9a4023ae0cfa3edbb7477b
Guilherme Nunes <guilherme.augd@gmail.com>
2025-03-07 15:13:09 -0300

    add component and code

---
+++ Part11/FlightControl/FlightControl.domain/Airplane.java
@@ -0,0 +1,28 @@
+package Part11.FlightControl.FlightControl.domain;
+
+
+package FlightControl.domain;
+
+
+public class Airplane {
+    private String id;
+    private int capacity;
+
+    public Airplane(String id, int capacity) {
+        this.id = id;
+        this.capacity = capacity;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " (" + this.capacity + " capacity)";
+    }
+}
