c109d49c8e92f429a5f93a30c871f48494a895f3
Guilherme Nunes <guilherme.augd@gmail.com>
2025-03-14 15:54:58 -0300

    implement exercise solution

---
+++ part12/HashMap/Program.java
@@ -0,0 +1,15 @@
+package part12.HashMap;
+
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("one", 1);
+        System.out.println(pair.getKey() + " -> " + pair.getValue());
+
+
+    }
+
+}
